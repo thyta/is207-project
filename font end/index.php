fc2cd8ab5e7e8cf7a99358f160996e5f120532b8
Tran Anh Thy <21521517@gm.uit.edu.vn>
2023-12-04 16:34:40 +0700

    fvck the footer

--- font end/index.php
+++ font end/index.php
@@ -12,9 +12,21 @@
     <!-- link to css -->
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/header.css">
+
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+    <!-- Css -->
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="./css/ds">
+    <!-- Feather icon -->
+    <script src="https://unpkg.com/feather-icons"></script>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
 </head>
 
 <body>
+    <!-- include the header -->
+
     <!-- temp carousel -- update later -->
 
     <!-- <div class="carousel-item active">
@@ -50,8 +62,15 @@
             </div>
         </div>
         <div class="col-sm-4">
-            <p>Với diện mạo đột phá có hình dáng dẹp dẹp như một lát bí đao, sữa chống nắng bí đao sẽ là món "trang sức" nhỏ gọn, tinh tế và không kém phần thời trang mà bạn sẽ luôn mang theo bên mình, bôi lại bất cứ khi nào cần thiết. Bảo vệ da khỏi tác hại của tia UVB, UVA và HEV với hiệu quả bảo vệ rất cao cùng finish ráo mịn rất tự nhiên, không bóng, không tạo vệt trắng mất thẩm mỹ.</p>
+            <p>Với diện mạo đột phá có hình dáng dẹp dẹp như một lát bí đao, sữa chống nắng bí đao sẽ là món "trang sức"
+                nhỏ gọn, tinh tế và không kém phần thời trang mà bạn sẽ luôn mang theo bên mình, bôi lại bất cứ khi nào
+                cần thiết. Bảo vệ da khỏi tác hại của tia UVB, UVA và HEV với hiệu quả bảo vệ rất cao cùng finish ráo
+                mịn rất tự nhiên, không bóng, không tạo vệt trắng mất thẩm mỹ.</p>
         </div>
+    </div>
+
+    <div>
+        <img src="img/index_bg_brand_triet_lí" alt="triet ly">
     </div>
 </body>
 
